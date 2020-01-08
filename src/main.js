8d951433ffb12d9af42579fc927aa1947c7073d8
songzhongyu <laotian2019szy@163.com>
2020-01-08 15:16:15 +0800

    项目初始化-封装注册vant组件模块

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载注册 vant 组件模块
+import './utils/register-vant'
 
 Vue.config.productionTip = false
 
